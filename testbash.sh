69e8ccf9e021c15a497aed075bac5b4349d5d700
George Crane <georgecrane@mac.com>
2022-10-18 05:48:00 -0400

    Update testbash.sh

--- testbash.sh
+++ testbash.sh
@@ -14,7 +14,7 @@ for filename in "$srcDir"/*.$srcExt; do
         echo "baseName=$baseName"
         echo "dest=$dest"
 
-        ffmpeg -y -ss 00:00:10 -i "$filename" -frames:v 1 "$dest"/"$baseName"."$destExt"
+        ffmpeg -y -ss 00:00:14 -i "$filename" -frames:v 1 "$dest"/"$baseName"."$destExt"
 
 done
 
@@ -24,8 +24,10 @@ done
 
 # ./testbash.sh "/Volumes/Seagate14b/Videos/Beth" "/Users/george/Sites/Gallery/Videos/Beth/thumb"
 
-./testbash.sh "/Users/george/Desktop/Family" "/Users/george/Desktop/Family/thumb"
+# ./testbash.sh "/Users/george/Desktop/Family" "/Users/george/Desktop/Family/thumb"
 
 # single call usage 
 
 # ffmpeg -y -ss 00:00:10 -i "$source" -frames:v 1 "$dest"
+
+# ./testbash.sh "/Users/george/Sites/CCLA_Photos/Videos/Events" "/Users/george/Sites/CCLA_Photos/Videos/Events/thumb"
